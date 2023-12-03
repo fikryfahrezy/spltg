476ec4a35c4422e48881b52114cdf2b918066f17
Fikry Fahrezy <ffahrezi69@gmail.com>
2023-12-03 23:13:53 +0700

    fix: signIn only with GitHub

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,5 +9,5 @@
 	<img src="https://cdn.pixabay.com/photo/2017/08/11/19/36/vw-2632486_1280.png" />
 {:else}
 	<p>Not signed in.</p>
-	<button on:click={() => signIn('github')}>Sign in</button>
+	<button on:click={() => signIn()}>Sign in</button>
 {/if}
