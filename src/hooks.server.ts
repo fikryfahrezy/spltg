db644a5a0a0b2a45bf67573b28966d2639ce17f6
Fikry Fahrezy <ffahrezi69@gmail.com>
2023-12-10 10:30:43 +0700

    feat: remove login with GitHub

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,9 +1,6 @@
 import { SvelteKitAuth } from '@auth/sveltekit';
-import GitHub from '@auth/core/providers/github';
 import Spotify from '@auth/core/providers/spotify';
 import {
-	GITHUB_ID,
-	GITHUB_SECRET,
 	SPOTIFY_CLIENT_ID,
 	SPOTIFY_CLIENT_SECRET,
 	SPOTIFY_AUTHORIZATION
@@ -11,7 +8,6 @@ import {
 
 export const handle = SvelteKitAuth({
 	providers: [
-		GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET }),
 		Spotify({
 			clientId: SPOTIFY_CLIENT_ID,
 			clientSecret: SPOTIFY_CLIENT_SECRET,
