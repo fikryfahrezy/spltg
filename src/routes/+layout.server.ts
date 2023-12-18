ef369844a8bcbc055d1488345052318cdc71a71d
Fikry Fahrezy <ffahrezi69@gmail.com>
2023-12-19 00:57:59 +0700

    refactor: remove `/auth/session` API route

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,21 +1,30 @@
 import type { LayoutServerLoad } from './$types';
+import { AUTH_SECRET } from '$env/static/private';
+import { decode } from '$lib/jwt';
+import { sessionKey } from '$lib/constants';
 import type { Session } from '../types/session';
 
-export const load: LayoutServerLoad = async ({ url, request }) => {
-	const res = await fetch(`${url.origin}/auth/session`, { headers: request.headers })
-		.then((res) => {
-			return res.json() as Session;
-		})
-		.then((session) => {
-			return {
-				session: session
-			};
-		})
-		.catch(() => {
-			return {
-				session: null
-			};
-		});
+export const load: LayoutServerLoad<{ session: Session | null }> = async ({ cookies }) => {
+	const sessionToken = cookies.get(sessionKey);
 
-	return res;
+	if (sessionToken === undefined) {
+		return {
+			session: null
+		};
+	}
+
+	const session = await decode({
+		salt: sessionKey,
+		secret: AUTH_SECRET,
+		token: sessionToken
+	});
+
+	return {
+		session: {
+			access_token: session?.access_token,
+			email: session?.email,
+			name: session?.name,
+			picture: session?.picture
+		}
+	};
 };
