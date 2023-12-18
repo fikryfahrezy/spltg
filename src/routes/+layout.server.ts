6fed64fbd8aac62dbf791eb2ffe50fc9c9b55d39
Fikry Fahrezy <ffahrezi69@gmail.com>
2023-12-19 01:03:26 +0700

    feat: implement refresh token rotation

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,10 +1,15 @@
 import type { LayoutServerLoad } from './$types';
-import { AUTH_SECRET } from '$env/static/private';
-import { decode } from '$lib/jwt';
+import { AUTH_SECRET, SPOTIFY_AUTH_REFRESH_TOKEN_PATH } from '$env/static/private';
+import { encode, decode } from '$lib/jwt';
 import { sessionKey } from '$lib/constants';
 import type { Session } from '../types/session';
+import type { SpotifyAccessTokenResponse } from '../types/spotify';
 
-export const load: LayoutServerLoad<{ session: Session | null }> = async ({ cookies }) => {
+export const load: LayoutServerLoad<{ session: Session | null }> = async ({
+	url,
+	request,
+	cookies
+}) => {
 	const sessionToken = cookies.get(sessionKey);
 
 	if (sessionToken === undefined) {
@@ -19,12 +24,61 @@ export const load: LayoutServerLoad<{ session: Session | null }> = async ({ cook
 		token: sessionToken
 	});
 
-	return {
-		session: {
-			access_token: session?.access_token,
-			email: session?.email,
-			name: session?.name,
-			picture: session?.picture
+	if (session === null) {
+		return {
+			session: null
+		};
+	}
+
+	try {
+		if (session.exp !== undefined && Date.now() < session.exp * 1000) {
+			return {
+				session: {
+					email: session.email,
+					name: session.name,
+					picture: session.picture,
+					access_token: session.access_token
+				}
+			};
 		}
-	};
+
+		const res = await fetch(`${url.origin}${SPOTIFY_AUTH_REFRESH_TOKEN_PATH}`, {
+			headers: request.headers
+		});
+		const newAccessToken: SpotifyAccessTokenResponse = await res.json();
+
+		const newSessionToken = await encode({
+			salt: sessionKey,
+			secret: AUTH_SECRET,
+			maxAge: newAccessToken.expires_in,
+			token: {
+				name: session?.name,
+				email: session?.email,
+				picture: session?.picture,
+				exp: newAccessToken.expires_in,
+				access_token: newAccessToken.access_token,
+				refresh_token: newAccessToken.refresh_token ?? session.refresh_token
+			}
+		});
+
+		cookies.set(sessionKey, newSessionToken, {
+			path: '/',
+			httpOnly: true
+		});
+
+		const newSession: Session = {
+			email: session.email,
+			name: session.name,
+			picture: session.picture,
+			access_token: newAccessToken.access_token
+		};
+
+		return {
+			session: newSession
+		};
+	} catch (e) {
+		return {
+			session: null
+		};
+	}
 };
