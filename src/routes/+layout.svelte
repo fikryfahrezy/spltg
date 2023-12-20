f3c254542eb68c54476057b44e6f67c33624bbb4
Fikry Fahrezy <ffahrezi69@gmail.com>
2023-12-20 22:47:17 +0700

    feat: setup unocss

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	import 'virtual:uno.css';
+	import '@unocss/reset/tailwind-compat.css';
+</script>
+
+<slot />
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import UnoCSS from 'unocss/vite';
+import extractorSvelte from '@unocss/extractor-svelte';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit(), UnoCSS({ extractors: [extractorSvelte()] })]
 });
